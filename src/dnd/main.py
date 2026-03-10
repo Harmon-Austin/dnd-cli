465b5dea58ec141fb97642197b9b1c530b1f8324
HarmonGAustin <harmongaustin@gmail.com>
2026-03-10 15:52:41 -0400

    Fixed typo

--- src/dnd/main.py
+++ src/dnd/main.py
@@ -9,7 +9,7 @@ app = typer.Typer()
 def roll(
         times: Annotated[int, typer.Option(help="How many times to perform the operation")] = 1,
         full: Annotated[bool, typer.Option(help="Displays each die roll")] = False,
-        dice: Annotated[List[str], typer.Argument(help="Dice to roll. Can add a modifier. (Spaces allowed)")] = "1d20"
+        dice: Annotated[List[str], typer.Argument(help="Dice to roll. Can add a modifier. (Spaces allowed)")] = ["1d20"]
         ):
     """
     Rolls dice using D&D notation.
